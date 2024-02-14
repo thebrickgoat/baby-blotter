52b94b5033799c3476b3804b5fd0b55aaf61e0ba
thebrickgoat <thebrickgoat@gmail.com>
2024-02-14 11:56:02 -0500

    mobile fixes

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
     return (
         <header className="bg-accent py-8">
-            <div className="container mx-auto">
+            <div className="container px-4 md:px-0 mx-auto">
                 <div className="text-white text-2xl font-bold text-right">❤️ ps happy valentines day</div>
             </div>
         </header>
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
     return (
         <header className="bg-accent py-8">
-            <div className="container mx-auto">
+            <div className="container px-4 md:px-0 mx-auto">
                 <div className="text-white text-2xl font-bold">Baby Blotter 🚨</div>
             </div>
         </header>
