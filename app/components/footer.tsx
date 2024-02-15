ead56dc98ed1d581e3eb0b255efc5f9dc78785fb
Cory Goad <thebrickgoat@gmail.com>
2024-02-14 21:20:37 -0500

    style stuff

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 export default function Footer() {
     return (
         <header className="bg-accent py-8">
-            <div className="container px-4 md:px-0 mx-auto">
-                <div className="text-white text-2xl font-bold text-right">❤️ ps happy valentines day</div>
+            <div className="container px-8 mx-auto flex items-center justify-between">
+                <div className="text-white text-xl font-bold">brought to you by zebra gum <a className="text-xs font-normal" href='thebrickgoat.com'>and thebrickgoat i guess </a></div>
+                <div className="text-white text-xl font-bold text-right">❤️ ps happy valentines day</div>
             </div>
         </header>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,9 @@ export default async function Home() {
     return `${formatedDate}, ${formatedTime}`;
   }
   return (
-    <div className="container px-4 md:px-0 mx-auto py-8 md:py-16 ">
+    <div className="container px-8 mx-auto py-8 ">
       <div className="hidden bg-accent-1 bg-accent-2 bg-accent-3 bg-accent-4 bg-accent-5" />
-      <h1 className="mb-6 md:mb-8 text-4xl">Latest Blots</h1>
+      <h1 className="mb-6 md:mb-8 text-6xl font-bold">Latest Blots</h1>
       <div className="">
         {reversedBlotters?.map((blotter) => (
           <div className={`p-8 my-8 first-of-type:mt-0 text-white rounded-md drop-shadow-xl	 bg-accent-${Math.floor(Math.random() * 4) + 2}`} key={blotter.id}>
