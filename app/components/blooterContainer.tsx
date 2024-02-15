ab355c51ef8e6f16f8edf34638bd6a72cd125c51
Cory Goad <thebrickgoat@gmail.com>
2024-02-15 17:36:04 -0500

    better scrollin

--- app/components/blooterContainer.tsx
+++ app/components/blooterContainer.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react';
+import { useState, useEffect, use } from 'react';
 
 interface Blotter {
     id: number;
@@ -21,27 +21,28 @@ export default function BlotterContainer({ blotters }: BlotterContainerProps) {
         const formattedTime = new Date(date).toLocaleTimeString();
         return `${formattedDate}, ${formattedTime}`;
     };
+    
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [currentPage]);
 
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
     const currentBlotters = blotters.slice(indexOfFirstItem, indexOfLastItem);
-
-    const paginate = (pageNumber: number) => {
-        setCurrentPage(pageNumber)
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    };
-
     const totalPages = Math.ceil(blotters.length / itemsPerPage);
     const visiblePages = 3;
     const firstVisiblePage = Math.max(1, currentPage - Math.floor(visiblePages / 2));
     const lastVisiblePage = Math.min(totalPages, firstVisiblePage + visiblePages - 1);
 
+    const paginate = (pageNumber: number) => {
+        setCurrentPage(pageNumber);
+    };
+
     return (
         <div className="">
             {currentBlotters.map((blotter: Blotter) => (
                 <div
-                    className={`p-8 my-8 first-of-type:mt-0 text-white rounded-md drop-shadow-xl bg-accent-${Math.floor(Math.random() * 4) + 2
-                        }`}
+                    className={`p-8 my-8 first-of-type:mt-0 text-white rounded-md drop-shadow-xl bg-accent-${Math.floor(Math.random() * 4) + 2}`}
                     key={blotter.id}
                 >
                     <div className="flex text-lg pb-4">{formatedDate(blotter.created_at)}</div>
@@ -55,7 +56,7 @@ export default function BlotterContainer({ blotters }: BlotterContainerProps) {
                 {currentPage !== 1 && (
                     <button
                         onClick={() => paginate(1)}
-                        className={`px-4 py-2 mx-1 $ bg-accent-2 text-white rounded-md drop-shadow-xl hover:bg-accent-dark`}
+                        className="px-4 py-2 mx-1 $ bg-accent-2 text-white rounded-md drop-shadow-xl md:hover:bg-accent-dark"
                     >
                         First
                     </button>
@@ -64,20 +65,19 @@ export default function BlotterContainer({ blotters }: BlotterContainerProps) {
                 {Array.from({ length: lastVisiblePage - firstVisiblePage + 1 }).map((_, index) => {
                     const pageNumber = firstVisiblePage + index;
                     return (
-                        <button
+                        <a
                             key={index}
                             onClick={() => paginate(pageNumber)}
-                            className={`px-4 py-2 mx-1 ${currentPage === pageNumber ? 'bg-accent  text-white' : 'bg-accent-white text-accent border border-accent hover:text-white'} rounded-md drop-shadow-xl hover:bg-accent-dark`}
+                            className={`px-4 py-2 mx-1 ${currentPage === pageNumber ? 'bg-accent text-white' : 'bg-accent-white text-accent border border-accent md:hover:text-white'} rounded-md drop-shadow-xl md:hover:bg-accent-dark`}
                         >
                             {pageNumber}
-                        </button>
+                        </a>
                     );
                 })}
                 {currentPage !== totalPages && (
-
                     <button
                         onClick={() => paginate(totalPages)}
-                        className={`px-4 py-2 mx-1 text-white bg-accent-2 rounded-md drop-shadow-xl hover:bg-accent-dark`}
+                        className="px-4 py-2 mx-1 text-white bg-accent-2 rounded-md drop-shadow-xl md:hover:bg-accent-dark"
                     >
                         Last
                     </button>
